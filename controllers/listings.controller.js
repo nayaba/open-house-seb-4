b24c854405bdf45781da697044316da3d767b7bf
Nabila Ayaba <nabilaayaba@gmail.com>
2025-02-06 14:10:36 +0300

    Finish favorite/unfavorite function

--- controllers/listings.controller.js
+++ controllers/listings.controller.js
@@ -34,11 +34,15 @@ const createListing = async (req, res) => {
 const show = async (req, res) => {
     try {
         const listing = await Listing.findById(req.params.listingId).populate('owner')
-        console.log(listing)
+        
+        const userHasFavorited = listing.favoritedByUsers.some((user) => user.equals(req.session.user._id))
+
         res.render('listings/show.ejs', {
             title: listing.streetAddress,
-            listing
+            listing,
+            userHasFavorited
         })
+
     } catch (error) {
         console.log(error)
         res.redirect('/')
@@ -93,6 +97,32 @@ const update = async (req, res) => {
     }
 }
 
+const addFavorite = async (req, res) => {
+    try {
+        const listing = await Listing.findByIdAndUpdate(req.params.listingId, {
+            $push: { favoritedByUsers: req.params.userId }
+        })
+        res.redirect(`/listings/${listing._id}`)
+       
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+}
+
+const removeFavorite = async (req, res) => {
+    try {
+        const listing = await Listing.findByIdAndUpdate(req.params.listingId, {
+            $pull: { favoritedByUsers: req.params.userId }
+        })
+        res.redirect(`/listings/${listing._id}`)
+       
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+}
+
 module.exports = {
     index,
     newListing,
@@ -101,4 +131,6 @@ module.exports = {
     deleteListing,
     edit,
     update,
+    addFavorite,
+    removeFavorite,
 }
--- server.js
+++ server.js
@@ -66,6 +66,8 @@ app.get('/listings/:listingId', listingsCtrl.show)
 app.delete('/listings/:userId/:listingId', listingsCtrl.deleteListing)
 app.get('/listings/:userId/:listingId/edit', listingsCtrl.edit)
 app.put('/listings/:userId/:listingId', listingsCtrl.update)
+app.post('/listings/:listingId/favorited-by/:userId', listingsCtrl.addFavorite)
+app.delete('/listings/:listingId/favorited-by/:userId', listingsCtrl.removeFavorite)
  
 app.listen(port, () => { 
     console.log(`The express app is ready on port ${port}`)
